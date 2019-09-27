52d74e9e6010a18c2c0780bc7d1be33b763a3ef7
Makarov Oleg <fantom2.00@mail.ru>
2019-09-27 22:33:00 +0300

    -

--- src/components/UserList/index.js
+++ src/components/UserList/index.js
@@ -2,6 +2,19 @@ import React from 'react';
 import './index.css';
 
 export default function UserList() {
+
+  const [RegStatus, setRegStatus] = React.useState(false)
+
+  React.useMemo(() => {
+    fetch('/currentUser')
+      .then(response => response.json())
+      .then(res => {
+        if (res.status_text === 'OK') {
+          setRegStatus(true)
+        }
+      })
+  })
+
   return (
     <div className="UserList">
         UserList
