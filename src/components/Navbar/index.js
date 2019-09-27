ca52329f76e404082ecbc8b8703e78ec7b4f5a6c
Makarov Oleg <fantom2.00@mail.ru>
2019-09-27 21:53:08 +0300

    Start vision

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.css';
+
+export default function Navbar() {
+  return (
+    <div className="Navbar">
+        Navbar
+    </div>
+    );
+}
+
---
+++ src/components/UserList/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.css';
+
+export default function UserList() {
+  return (
+    <div className="UserList">
+        UserList
+    </div>
+    );
+}
