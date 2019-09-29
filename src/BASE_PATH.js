439b4d147bed534bf00851413f541d9ab4a5b508
Makarov Oleg <fantom2.00@mail.ru>
2019-09-29 09:32:13 +0300

    Orders work

--- src/BASE_PATH.js
+++ src/BASE_PATH.js
@@ -1,3 +1,3 @@
-// const BASE_PATH = "http://tomcat8.rs-soft.site/hackathon-app-1.0-SNAPSHOT"
-const BASE_PATH = "http://10.70.9.52:8080"
+const BASE_PATH = "http://tomcat8.rs-soft.site/hackathon-app-1.0-SNAPSHOT"
+// const BASE_PATH = "http://10.70.9.52:8080"
 export default BASE_PATH
--- src/components/OrdersBlock/index.js
+++ src/components/OrdersBlock/index.js
@@ -6,15 +6,17 @@ export default function UserList() {
 
   const [id, setId] = React.useState(1)
   const [list, setList] = React.useState([])
+  const [amountActiveTasks, setAmountActiveTasks] = React.useState(0)
 
   React.useMemo(() => {
     fetch(BASE_PATH + '/volunteer/'+id+'/activeTasks')
       .then(response => response.json())
       .then(res => {
         setList(res)
+        setAmountActiveTasks(res.length)
         console.log("res: ", res)
       })
-  },[])
+  },[amountActiveTasks])
 
 
   const taskDone = (tasksId) =>{
@@ -29,29 +31,39 @@ export default function UserList() {
     })
       .then(response => response.json())
       .then(res => {
-        console.log(res)
+        console.log("res", res)
       })
       .catch(err => console.log(err))
+
+      setAmountActiveTasks(amountActiveTasks-1)
   }
 
   const List = list.map((value) => {
       return(
-        <div className="card">
-          <span className="taskId">value.taskId</span>
-          <span className="taskName">value.taskName</span>
-          <span className="taskDescription">value.taskDescription</span>
-          <span className="taskRating">value.taskRating</span>
+        <div className="card" key={value.id}>
+          <div className="infoLine">
+            <span className="taskId">{value.id}</span>
+            <span className="taskName emetedText midlleText bebas">{value.taskName}</span>
+          </div>
+          <span className="taskDescription">{value.taskDescription}</span>
+          <span className="taskRating margVert">Raiting: {value.taskRating}</span>
 
           <div className="acciveConteiner">
-            <div className="acciveButtom" onClick={() => taskDone(value.taskId)}>Завершено</div>
+            <div className="acciveButtom" onClick={() => taskDone(value.id)}>Завершено</div>
           </div>
         </div>
       )
   })
 
   return (
-    <div className="deskArea">
-        {List}
-    </div>
+    <>
+      <div className="deksArea">
+          {List}
+      </div>
+     
+      <div className="actveTasksAmount">
+        <span className="emetedText margVert">Amount active tasks: {amountActiveTasks}</span>
+      </div>
+     </>
     );
 }
